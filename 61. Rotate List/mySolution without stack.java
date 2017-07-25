6f478fbd916464251f4ea4a3aedb7a15e7c0761b
wusai2333 <wusai2333@hotmail.com>
2017-07-25 13:41:42 -0700

    rotatelinkedlist  update

--- 61. Rotate List/mySolution without stack.java
+++ 61. Rotate List/mySolution without stack.java
@@ -1,15 +1,16 @@
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0) {
-            retuen head;
+            return head;
         }
         ListNode tail = head;
-        int size = 0;
-        while (tail != null) {
+        int size = 1;
+        while (tail.next != null) {
             tail = tail.next;
             size ++;
         }
         tail.next = head;
+        // create a circle; then break it between the break point tail;
         k = k >= size ? k % size: k;
         for (int i = 0; i < size - k; i ++) {
             tail = tail.next;
