015fd6e744f6fc681dbda2b60f4e0df85d9173d5
wusai2333 <wusai2333@hotmail.com>
2017-07-25 00:44:35 -0700

    Design Twitter last update

--- 355. Design Twitter/mySolution.java
+++ 355. Design Twitter/mySolution.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Twitter {
 
@@ -15,23 +16,25 @@ public class Twitter {
     private class Tweet {
         public int id;
         public int time;
+        public Tweet next;
 
         public Tweet(int id) {
             this.id = id;
             time = timeStamp++;
+            next = null;
         }
     }
 
     private class User {
-        public id;
+        public int id;
         public Set<Integer> followed;
-        public List<Tweet> tweets;
+        public Tweet tweet_head;;
         
         public User(int id) {
             this.id = id;
             followed = new HashSet<>();
             follow(id);
-            tweets = new LinkedList<>();
+            tweet_head = null;
         }
 
         public void follow(int id) {
@@ -43,29 +46,47 @@ public class Twitter {
         }
 
         public void post(int id) {
-            tweets.add(new Tweet(id));
+            Tweet t = new Tweet(id);
+            t.next = tweet_head;
+            tweet_head = t;
         }
     }
     /** Initialize your data structure here. */
     public Twitter() {
         userMap = new HashMap<>();
-        timestamp = 0;
     }
     
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        User user = userMap.get(userId);
-        user.post
+        if(!userMap.containsKey(userId)) {
+            User user = new User(userId);
+            userMap.put(userId, user);
+        }
+        userMap.get(userId).post(tweetId);
     }
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
         List<Integer> res = new ArrayList<>();
-        if (UserId_TweetId.containsKey(userId)) {
-            List<Integer> list = UserId_TweetId.get(userId);
-            for(int i = list.size()-1; i >= list.size() - 10; i --) {
-                res.add(list.get(i));
-                if (i == 0) break;
+        if (!userMap.containsKey(userId)) {
+            return res;
+        }
+        Set<Integer> users = userMap.get(userId).followed;
+        PriorityQueue<Tweet> q = new PriorityQueue<>(users.size(), (a,b)->(b.time - a.time));
+        for(int user: users) {
+            Tweet t = userMap.get(user).tweet_head;
+            //
+            if (t != null) {
+                q.add(t);
+            }
+        }
+        int n = 0;
+        while(!q.isEmpty() && n < 10) {
+            Tweet t = q.poll();
+            res.add(t.id);
+            n++;
+            if (t.next != null) {
+                q.add(t.next);
             }
         }
         return res;
@@ -73,32 +94,22 @@ public class Twitter {
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        if (UserId_FollowId.containsKey(followerId)) {
-            HashSet<Integer> set = UserId_FollowId.get(followerId);
-            if (set.contains(followeeId)) {
-                return;
-            } else {
-                set.add(followeeId);
-            }
-        } else {
-            HashSet<Integer> set = new HashSet<>();
-            set.add(followeeId);
-            UserId_FollowId.put(followerId, set);
+        if (!userMap.containsKey(followerId)) {
+            User u = new User(followerId);
+            userMap.put(followerId, u);
         }
+        if(!userMap.containsKey(followeeId)) {
+            User u = new User(followeeId);
+            userMap.put(followeeId, u);
+        }
+        userMap.get(followerId).follow(followeeId);
     }
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if (UserId_FollowId.containsKey(followerId)) {
-            HashSet<Integer> set = UserId_FollowId.get(followerId);
-            if (!set.contains(followeeId)) {
-                return;
-            } else {
-                set.remove(followeeId);
-            }
-        } else {
+        if (!userMap.containsKey(followerId) || followerId == followeeId) 
             return;
-        }       
+        userMap.get(followerId).unfollow(followeeId);
     }
 }
 
