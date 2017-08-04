f8bc48cd60f1f78c990ec1f8e4bbc7de952fd59d
wusai2333 <wusai2333@hotmail.com>
2017-08-03 19:17:11 -0700

    Unique path I and II

--- 62. Unique Paths/dp solution.java
+++ 62. Unique Paths/dp solution.java
@@ -12,6 +12,6 @@ public class Solution {
                 dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
-        return dp[m-1][n-1];   
+        return dp[m-1][n-1];
     }
 }
--- 63. Unique Paths II/mySolution.java
+++ 63. Unique Paths II/mySolution.java
@@ -1,21 +1,16 @@
 
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for (int i = 0; i < obstacleGrid.length; i++) {
-            dp[i][0] = 1;
-            if (obstacleGrid[i][0] == 1) dp[i][0] = 0;
-        }
-        for (int i = 0; i < obstacleGrid[0].length; i++) {
-            dp[0][i] = 1;
-            if (obstacleGrid[0][i] == 1) dp[0][i] = 0;
-        }
-        for (int i = 1; i < obstacleGrid.length; i++) {
-            for (int j = 1; j < obstacleGrid[0].length; j++) {
-                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m+1][n+1];
+        dp[0][1] = 1;
+       
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (obstacleGrid[i-1][j-1] == 1) dp[i][j] = 0;
                 else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return dp[obstacleGrid.length][obstacleGrid[0].length];
+        return dp[m][n];
     }
 }
