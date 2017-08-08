bbb06fd3b3d2edb5e3f52d2ed7c5c39d50d319f2
wusai2333 <wusai2333@hotmail.com>
2017-08-07 19:50:59 -0700

    backTrack

--- 216. Combination Sum III/mySolution.java
+++ 216. Combination Sum III/mySolution.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    List<List<Integer>> res = new List<>();
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
         if (n <= 0 || k <= 0) return res;
-        boolean[] used = new boolean[10];
-        
+        backTrack(new ArrayList<Integer>(), k, 1, n);
+        return res;
     }
-    public void backTrack(boolean[] used, )
+    public void backTrack(List<Integer> comb, int k, int start, int n) {
+        if (comb.size() == k && n == 0) {
+            res.add(new ArrayList<Integer>(comb));
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            comb.add(i);
+            backTrack(comb, k, i+1, n-i);
+            comb.remove(comb.size()-1);
+        }
+    }
 }
