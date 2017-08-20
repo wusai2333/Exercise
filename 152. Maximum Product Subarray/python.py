c4588f42a804d88abcf1d85051253bdf3982f922
wusai2333 <wusai2333@hotmail.com>
2017-08-19 18:12:49 -0700

    python keep min and max solution

--- 152. Maximum Product Subarray/python.py
+++ 152. Maximum Product Subarray/python.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution(object):
     def maxProduct(self, nums):
         """
@@ -17,24 +16,4 @@ class Solution(object):
             minherepre = minhere
             
         return maxsofar
-=======
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        maxherepre = nums[0]
-        minherepre = nums[0]
-        maxsofar = nums[0]
-        
-        for num in nums[1:]:
-            maxhere = max(max(maxherepre * num, minherepre * num), num)
-            minhere = min(min(minherepre * num, maxherepre * num), num)
-            maxsofar = max(maxhere, minhere, maxsofar)
-            maxherepre = maxhere
-            minherepre = minhere
-            
-        return maxsofar
->>>>>>> eb4f20642d79ed3df07dab1830859263475bf5e5
         
