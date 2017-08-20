cf022f165f0e812329ba62ea878df96677ea7887
wusai2333 <wusai2333@hotmail.com>
2017-08-19 17:34:32 -0700

    Math solution update

--- 343. Integer Break/mySolution.java
+++ 343. Integer Break/mySolution.java
@@ -2,6 +2,8 @@ import java.util.LinkedList;
 
 class Solution {
     public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
         int product = 1;
         while (n > 4) {
             product *= 3;
