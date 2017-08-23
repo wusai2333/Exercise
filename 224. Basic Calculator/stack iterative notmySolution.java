774130e0810d176f9a76ff21ca9eedaf763b84aa
wusai2333 <wusai2333@hotmail.com>
2017-08-23 16:40:59 -0700

    iterative stack calculator

--- 224. Basic Calculator/stack iterative notmySolution.java
+++ 224. Basic Calculator/stack iterative notmySolution.java
@@ -7,15 +7,31 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
-                number += 10*number + Character.getNumericValue(c);
+                number = 10*number + Character.getNumericValue(c);
             } else if (c == '+') {
                 result += sign * number;
                 number = 0;
                 sign = 1;
-            } else if (c == '-') [
+            } else if (c == '-') {
                 result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                // we push the result first, then sign;
+                stack.push(result);
+                stack.push(sign);
+                // reset the result and sign for the value in the parenthesis
+                sign = 1;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
                 
-            ]
         }
+        if (number != 0) result += sign * number;
+        return result;
     }
 }
