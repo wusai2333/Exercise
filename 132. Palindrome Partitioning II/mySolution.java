1a0d3c67398336d6c6edab066a47d181847be5e1
wusai2333 <wusai2333@hotmail.com>
2017-08-26 17:26:25 -0700

    palindrome partitioning

--- 132. Palindrome Partitioning II/mySolution.java
+++ 132. Palindrome Partitioning II/mySolution.java
@@ -1,27 +1,23 @@
 class Solution {
     public int minCut(String s) {
-        int[] dp = new int[s.length()+1];
-        boolean[][] pal = new boolean[s.length()+1][s.length()+1];
-        for (int i = 1; i <= s.length(); i++) {
-            dp[i] = i-1;
+        int[] dp = new int[s.length()];
+        boolean[][] pal = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i] = i;
         }
-        for (int i = 1; i <= s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             for (int j = i; j >= 0; j--) {
-                if (s.charAt(i) == s.charAt(j))
+                if (s.charAt(i) == s.charAt(j) && (i - j < 2 || pal[j+1][i-1])) {
+                    pal[j][i] = true;
+                    if (j == 0)
+                        dp[i] = 0;
+                    else if (dp[j-1] + 1 < dp[i]) {
+                        dp[i] = dp[j-1] + 1;
+                    }
+                }
             }
         }
-        return dp[s.length()];
+        return dp[s.length()-1];
     }
 
-    private boolean isPalindrome(String s) {
-        int start = 0, end = s.length()-1;
-        while (start < end) {
-            if (s.charAt(start++) != s.charAt(end--)) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
-
-res.stream().mapToInt(i->i).toArray();
