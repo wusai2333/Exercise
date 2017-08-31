3fa9957081e77b1984b468884c3da90d21feff67
wusai2333 <wusai2333@hotmail.com>
2017-08-31 14:10:35 -0700

    DFS topological order if hasCycle return ""

---
+++ 269. Alien Dictionary/DFS build graph.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Solution {
+    private final int N = 26;
+    public String alienOrder(String[] words) {
+        boolean[][] adj = new boolean[N][N];
+        int[] visited = new int[N];
+        buildGraph(words, adj, visited);
+        /**
+         * A topological order is possible if and only if the graph has no directed
+         * cycles;
+         * visited[i] = -1 Not exist;
+         * visited[i] = 0 exsit , Non-visited
+         * visited[i] = 1 visiting
+         * visited[i] = 2 visited
+         */
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            if (visited[i] == 0) {
+                if (!dfs(adj, visited, sb, i)) return "";
+            }
+        }
+        return sb.toString();
+    }
+
+    public boolean dfs(boolean[][] adj, int[] visited, StringBuilder sb, int i) {
+        visited[i] = 1;
+        for (int j = 0; j < N; j++) {
+            if (adj[i][j]) {
+                if (visited[j] == 1) return false; // cycle 1 -> 1;
+                if (visited[j] == 0) {
+                    if (!dfs(adj, visited, sb, j)) return false;
+                }
+            }
+        }
+        visited[i] = 2;
+        sb.append((char)i + 'a');
+        return true;
+    }
+
+    public void buildGraph(String[] words, boolean[][] adj, int[] visited) {
+        Arrays.fill(visited, -1);
+        for (int i = 0; i < words.length; i++) {
+            for (char c : words[i].toCharArray())
+                visited[c - 'a'] = 0;
+            if (i > 0) {
+                String w1 = words[i-1], w2 = words[i];
+                int len = Math.min(w1.length(), w2.length());
+                for (int j = 0; j < len; j++) {
+                    char c1 = w1.charAt(j);
+                    char c2 = w2.charAt(j);
+                    if (c1 != c2) {
+                        adj[c1 - 'a'][c2 - 'a'] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
