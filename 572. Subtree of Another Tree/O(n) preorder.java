4a53a31d6b527ae3238b5097c3aa86af0934777b
wusai2333 <wusai2333@hotmail.com>
2017-09-03 20:24:09 -0700

    O(n) using KMP

--- 572. Subtree of Another Tree/O(n) preorder.java
+++ 572. Subtree of Another Tree/O(n) preorder.java
@@ -13,14 +13,49 @@ public class Solution {
         while (!stack.empty()) {
             TreeNode node = stack.pop();
             if (node == null) {
-                sb.append("#");
+                sb.append(",#");
             }
             else {
-                sb.append(node.val);
+                sb.append("," + node.val);
                 stack.push(node.left);
                 stack.push(node.right);
             }
         }
         return sb.toString();
+    }
+
+    private boolean kmp(String s, String t) {
+        int i = 0, j = 0;
+        int n = s.length();
+        int m = t.length();
+        if (m == 0) return true;
+        int[] next = new int[m];
+        getNext(t, next);
+        while (i < n && j < m) {
+            if (j == -1 || s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j == m) return true;
+        else return false;
+    }
+
+    private void getNext(String t, int[] next) {
+        int len = t.length();
+        next[0] = -1;
+        int k = -1;
+        int j = 0;
+        while (j < len - 1) {
+            if (k == -1 || t.charAt(k) == t.charAt(j)) {
+                ++k;
+                ++j;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
+        }
     }
 }
